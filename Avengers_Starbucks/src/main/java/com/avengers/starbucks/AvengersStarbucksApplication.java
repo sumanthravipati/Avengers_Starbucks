24114affb956ed8a5a1d046dcaf846651e60d356
sumanthravipatirr <sumanthravipatirr@gmail.com>
2019-04-24 10:07:34 -0700

    Starbucks Final Project Creation

---
+++ Avengers_Starbucks/src/main/java/com/avengers/starbucks/AvengersStarbucksApplication.java
@@ -0,0 +1,13 @@
+package com.avengers.starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersStarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersStarbucksApplication.class, args);
+	}
+
+}
---
+++ Avengers_Starbucks/src/test/java/com/avengers/starbucks/AvengersStarbucksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.avengers.starbucks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvengersStarbucksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
