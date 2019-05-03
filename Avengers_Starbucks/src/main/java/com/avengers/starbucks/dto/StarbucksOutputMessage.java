d5d981ee46f765759b346e2842faa9f9d8c4d6f6
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-03 00:12:35 -0700

    Solved spell-mistake

--- Avengers_Starbucks/src/main/java/com/avengers/starbucks/dto/StarbucksOutputMessage.java
+++ Avengers_Starbucks/src/main/java/com/avengers/starbucks/dto/StarbucksOutputMessage.java
@@ -2,23 +2,26 @@ package com.avengers.starbucks.dto;
 
 import java.io.Serializable;
 
-public class StarbucksOutputMessage implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String successResonse;
-	private String errorResponse;
-	
-	public String getSuccessResonse() {
-		return successResonse;
-	}
-	public void setSuccessResonse(String successResonse) {
-		this.successResonse = successResonse;
-	}
-	public String getErrorResponse() {
-		return errorResponse;
-	}
-	public void setErrorResponse(String errorResponse) {
-		this.errorResponse = errorResponse;
-	}
+public class StarbucksOutputMessage implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private String successResponse;
+  private String errorResponse;
+
+  public String getSuccessResponse() {
+    return successResponse;
+  }
+
+  public void setSuccessResponse(String successResponse) {
+    this.successResponse = successResponse;
+  }
+
+  public String getErrorResponse() {
+    return errorResponse;
+  }
+
+  public void setErrorResponse(String errorResponse) {
+    this.errorResponse = errorResponse;
+  }
 }
