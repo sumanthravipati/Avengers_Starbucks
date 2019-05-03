c67e21d4841e49ea989fecd80699e37ca930e74d
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-03 00:04:10 -0700

    POJO to handle order request

---
+++ Avengers_Starbucks/src/main/java/com/avengers/starbucks/dto/OrderRequest.java
@@ -0,0 +1,35 @@
+package com.avengers.starbucks.dto;
+
+import com.avengers.starbucks.model.ProductRequest;
+
+import java.util.List;
+
+
+public class OrderRequest {
+
+  private List<ProductRequest> products;
+  private String emailId;
+
+
+  public OrderRequest(List<ProductRequest> products, String emailId) {
+    this.products = products;
+    this.emailId = emailId;
+  }
+
+  public String getEmailId() {
+    return emailId;
+  }
+
+  public void setEmailId(String emailId) {
+    this.emailId = emailId;
+  }
+
+  public List<ProductRequest> getProducts() {
+    return products;
+  }
+
+  public void setProducts(List<ProductRequest> products) {
+    this.products = products;
+  }
+
+}
