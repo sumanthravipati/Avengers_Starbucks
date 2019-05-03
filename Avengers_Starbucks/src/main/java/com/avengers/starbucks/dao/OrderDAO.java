4f3dbdf4e06382762dad7e5bd08cee92a04e7d89
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-05-03 00:10:48 -0700

    Db operation to place order

---
+++ Avengers_Starbucks/src/main/java/com/avengers/starbucks/dao/OrderDAO.java
@@ -0,0 +1,13 @@
+package com.avengers.starbucks.dao;
+
+import com.avengers.starbucks.model.Product;
+
+public interface OrderDAO {
+
+  boolean insertOrder(String emailId, String orderDescription, float billingAmt);
+
+  Product getProductDetail(int productId);
+
+  void updateStock(int productId, int quantity);
+
+}
